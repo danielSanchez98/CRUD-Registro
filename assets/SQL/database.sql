c52a22db9779df9a7d0ee8df41d32fd71f084f4b
danielSanchez98 <ds980220@gmail.com>
2020-04-23 14:56:54 -0500

    Script Base de Datos CRUD Registro

---
+++ assets/SQL/database.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE crud_registro CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+USE crud_registro;
+
+CREATE TABLE registros(
+    idRegistro INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    apellido VARCHAR(255) NOT NULL,
+    direccion VARCHAR(255) NOT NULL,
+    telefono VARCHAR(255) NOT NULL
+
+);
+
